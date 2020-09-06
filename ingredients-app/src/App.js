af77692eba57ae979bdab24ef336204b6df11cc3
kester-ng <kester.ng@visenze.com>
2020-09-07 02:14:34 +0800

    Added an add button for adding

--- ingredients-app/src/App.js
+++ ingredients-app/src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import { Table, Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'; 
+import { Table, Button, Modal, ModalBody, ModalFooter, ModalHeader, Input, FormGroup, Label } from 'reactstrap'; 
 import axios from 'axios';
 
 class App extends Component {
   state = {
     ingredients: [],
-    addModal: false
+    addModal: false,
+    newIngredientData: {
+      name: '',
+      price: '',
+      stock: ''
+    }
+  }
+
+  // POST to AWS lambda
+  addIngredient() {
+    axios.post("https://pyyz2y0tzg.execute-api.us-east-1.amazonaws.com/dev/ingredients", this.state.newIngredientData, {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    }).then((response) => {
+      console.log(response.data); // check to see if it is there
+      let { ingredients } = this.state;
+      ingredients.push(response.data.ingredients);
+      this.setState({
+        addModal: false,
+        newIngredientData: {
+          name: '',
+          price: '',
+          stock: ''
+        }
+      });
+    });
   }
 
   componentWillMount() {
@@ -45,12 +70,35 @@ class App extends Component {
       <div className="App Container">
         <Button color="primary" onClick={this.toggleAddModal.bind(this)}>Add</Button>
         <Modal isOpen={this.state.addModal} toggle={this.toggleAddModal.bind(this)}>
-        <ModalHeader toggle={this.toggleAddModal.bind(this)}>Modal title</ModalHeader>
+        <ModalHeader toggle={this.toggleAddModal.bind(this)}>Add a new ingredient or food item</ModalHeader>
           <ModalBody>
-            Cats are the cutest thing alive!
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input id="name" value={this.state.newIngredientData.name} onChange={(e) => {
+                let { newIngredientData } = this.state;
+                newIngredientData.name = e.target.value;
+                this.setState({ newIngredientData });
+              }}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="price">Price</Label>
+              <Input id="price" value={this.state.newIngredientData.price} onChange={(e) => {
+                let { newIngredientData } = this.state;
+                newIngredientData.price = e.target.value;
+                this.setState({ newIngredientData });
+              }}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="stock">Stock</Label>
+              <Input id="stock" value={this.state.newIngredientData.stock} onChange={(e) => {
+                let { newIngredientData } = this.state;
+                newIngredientData.stock = e.target.value;
+                this.setState({ newIngredientData });
+              }}/>
+            </FormGroup>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggleAddModal.bind(this)}>Do Something</Button>{' '}
+            <Button color="primary" onClick={this.addIngredient.bind(this)}>Do Something</Button>{' '}
             <Button color="secondary" onClick={this.toggleAddModal.bind(this)}>Cancel</Button>
           </ModalFooter>
         </Modal>
